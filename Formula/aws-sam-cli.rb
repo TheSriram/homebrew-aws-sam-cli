6b869c9e234ec11cf6bb11aa69993cb0299b41c9
Sriram Madapusi Vasudevan <3770774+TheSriram@users.noreply.github.com>
2018-09-21 12:15:22 -0700

    Add bintray with a sierra bottle.

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -6,7 +6,12 @@ class AwsSamCli < Formula
   url "https://github.com/awslabs/aws-sam-cli/archive/v0.6.0.tar.gz"
   sha256 "f85762aba829525eb8c6a52d354ef7254ed37e5bc8a7389885fd0daebfea1c96"
   head "https://github.com/TheSriram/aws-cli.git", :branch => "develop"
-  bottle :unneeded
+  bottle do
+    root_url "https://dl.bintray.com/thesriram/aws-sam-cli"
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "e861e50c9a0b5b595dd4c0a548796d05cc449aa64dc14e6a989e00ba92aa7700" => :sierra
+  end
 
   # Some AWS APIs require TLS1.2, which system Python doesn't have before High
   # Sierra
