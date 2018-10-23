8f33fbd826d58f9f7cdb166b7182e9b73a8a204c
Sriram Madapusi Vasudevan <3770774+TheSriram@users.noreply.github.com>
2018-10-22 17:17:49 -0700

    correct repo title

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -5,7 +5,7 @@ class AwsSamCli < Formula
   homepage "https://mysamwebpage.com/aws-sam-cli"
   url "https://github.com/awslabs/aws-sam-cli/archive/v0.6.0.tar.gz"
   sha256 "f85762aba829525eb8c6a52d354ef7254ed37e5bc8a7389885fd0daebfea1c96"
-  head "https://github.com/TheSriram/aws-cli.git", :branch => "develop"
+  head "https://github.com/TheSriram/aws-sam-cli.git", :branch => "develop"
   bottle do
     root_url "https://dl.bintray.com/thesriram/aws-sam-cli"
     cellar :any_skip_relocation
