16eb54f73094d6e53991d0631bd071a5dbd3e2fe
Sriram Madapusi Vasudevan <3770774+TheSriram@users.noreply.github.com>
2018-12-07 09:44:13 -0800

    update desc

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -1,7 +1,7 @@
 class AwsSamCli < Formula
   include Language::Python::Virtualenv
 
-  desc "SAM command line interface"
+  desc "AWS SAM CLI"
   homepage "https://docs.aws.amazon.com/serverless-application-model/latest/developerguide"
   url "https://files.pythonhosted.org/packages/8f/29/a0cb402d1875d0b35aa7c99b644a75fc72762dce25bc4fce7d62e1b70825/aws-sam-cli-0.8.1.tar.gz"
   sha256 "cdcc9864f4b37865bf5669df9a91a4a0d0ed4ab4975ba4ad9e4e4b84012c3345"
