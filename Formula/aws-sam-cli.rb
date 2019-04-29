7656bbd5aaa0b4059d4c4eb9cfc88b94f55420e3
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2019-04-29 12:39:40 -0700

    old bottle`

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -1,18 +1,20 @@
 class AwsSamCli < Formula
   include Language::Python::Virtualenv
 
-  desc "AWS SAM CLI 🐿 is a tool for local development and testing of Serverless applications"
-  homepage "https://github.com/awslabs/aws-sam-cli/"
-  url "https://files.pythonhosted.org/packages/92/df/3e8c8a65075d810f8f35157d60c06461061f51c3713c47ca3afaef2c5b89/aws-sam-cli-0.15.0.tar.gz"
-  sha256 "cdfdb39c1b0fc8925b8e24719d934c2e676cc675b10532d038c1fb90dd8d3786"
-  head "https://github.com/awslabs/aws-sam-cli.git", :branch => "develop"
-
+  desc "SAM command line interface"
+  homepage "https://mysamwebpage.com/aws-sam-cli"
+  url "https://github.com/awslabs/aws-sam-cli/archive/v0.6.0.tar.gz"
+  sha256 "f85762aba829525eb8c6a52d354ef7254ed37e5bc8a7389885fd0daebfea1c96"
+  head "https://github.com/TheSriram/aws-cli.git", :branch => "develop"
   bottle do
     root_url "https://dl.bintray.com/thesriram/aws-sam-cli"
-    cellar :any
-    sha256 "129f48648395f3bab0bc865c70f4cba33302525d47f1252196f6de1c32b59ec1" => :sierra_or_later
+    cellar :any_skip_relocation
+    rebuild 1
+    sha256 "e861e50c9a0b5b595dd4c0a548796d05cc449aa64dc14e6a989e00ba92aa7700" => :sierra
   end
 
+  # Some AWS APIs require TLS1.2, which system Python doesn't have before High
+  # Sierra
   depends_on "python"
 
   def install
