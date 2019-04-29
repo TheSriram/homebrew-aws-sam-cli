8b21c107adb9717b76b520c7b57cde4ff3c334c9
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2019-04-29 12:44:25 -0700

    change url

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -3,8 +3,8 @@ class AwsSamCli < Formula
 
   desc "SAM command line interface"
   homepage "https://mysamwebpage.com/aws-sam-cli"
-  url "https://github.com/awslabs/aws-sam-cli/archive/v0.6.0.tar.gz"
-  sha256 "f85762aba829525eb8c6a52d354ef7254ed37e5bc8a7389885fd0daebfea1c96"
+  url "https://github.com/awslabs/aws-sam-cli/archive/v0.15.0.tar.gz"
+  sha256 "f23eec0f00826d14554283c93aedf3202193d74865af50e0e8ffb86df7c22d4d"
   head "https://github.com/TheSriram/aws-cli.git", :branch => "develop"
   bottle do
     root_url "https://dl.bintray.com/thesriram/aws-sam-cli"
