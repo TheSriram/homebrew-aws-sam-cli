8dcc003be0a4f717dbc0d4b5ad2d286e8fe03313
Sriram Madapusi Vasudevan <srirammv@amazon.com>
2019-06-04 09:01:25 -0700

    fix: go to latest release

--- Formula/aws-sam-cli.rb
+++ Formula/aws-sam-cli.rb
@@ -3,11 +3,11 @@ class AwsSamCli < Formula
 
   desc "SAM command line interface"
   homepage "https://mysamwebpage.com/aws-sam-cli"
-  url "https://github.com/awslabs/aws-sam-cli/archive/v0.15.0.tar.gz"
-  sha256 "f23eec0f00826d14554283c93aedf3202193d74865af50e0e8ffb86df7c22d4d"
+  url "https://github.com/awslabs/aws-sam-cli/archive/v0.16.1.tar.gz"
+  sha256 "b863265bb768147fd2bac6d424581af2ace8855d8d855e324a49f39a8c4c066a"
   head "https://github.com/TheSriram/aws-cli.git", :branch => "develop"
   bottle do
-    root_url "https://github.com/TheSriram/homebrew-aws-sam-cli/releases/download/v0.15.0/"
+    root_url "https://github.com/TheSriram/homebrew-aws-sam-cli/releases/download/v0.16.1/"
     sha256 "646a73abc522f3365ebfef06215b7d88b35c8b6c2ad70dbcd267f98fd994e4d9" => :el_capitan
     sha256 "01c8c4ab9317db6bb2b74ce64918872c616aa0c73db9111dd19292fb096e78f4" => :x86_64_linux
   end
